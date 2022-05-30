b59f031fb253545750702b21c515fadc04b70a20
alexia2007 <99951243+alexia2007@users.noreply.github.com>
2022-05-30 12:48:27 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
+let strip: neopixel.Strip = null
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 20; index++) {
         music.playTone(494, music.beat(BeatFraction.Whole))
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
         music.playTone(988, music.beat(BeatFraction.Whole))
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
     }
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 20; index++) {
         music.playTone(262, music.beat(BeatFraction.Whole))
+        strip.showColor(neopixel.colors(NeoPixelColors.Orange))
         music.playTone(494, music.beat(BeatFraction.Whole))
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
     }
 })
